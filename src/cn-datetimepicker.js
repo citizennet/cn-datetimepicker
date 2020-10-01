9335c843e616e318928aa9968973d7726c3646c6
jocteau <jocteau@citizennet.com>
2020-10-01 02:56:28 -0700

    wip

--- src/cn-datetimepicker.js
+++ src/cn-datetimepicker.js
@@ -117,7 +117,7 @@
       //////////
 
       $scope.lostFocus = () => {
-        consoel.log('lostFocus', $scope)
+        console.log('lostFocus', $scope);
         $scope.ngModel = $scope.localNgModel;
         if($scope.onChange) {
           $scope.onChange({$value: newVal});
